a793d768fc96072a595f7c050417bfe1359a3189
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2014-02-28 16:57:22 +0100

    Use oasis to compile

--- Makefile
+++ Makefile
@@ -1,10 +1,45 @@
+NAME = $(shell oasis query name)
+DIR = $(NAME)-$(shell oasis query version)
+TARBALL = $(DIR).tar.gz
 
-pkg:
-	ocamlbuild -use-ocamlfind tgeoip.{mli,cmi,cmx,cma,a,cmxa,cmxs} dlltgeoip.so META
+DISTFILES = AUTHORS.txt INSTALL.txt README.txt \
+  Makefile myocamlbuild.ml _oasis _tags \
+  $(wildcard $(addprefix src/,*.ml *.mli *.clib *.mllib *.c *.h))
 
-install:
-	ocamlfind install tgeoip META _build/src/tgeoip.{mli,cmi,cmx,cma,a,cmxa,cmxs} _build/src/dlltgeoip.so
+all byte native setup.log: setup.data
+	ocaml setup.ml -build
 
-test:
-	ocamlbuild -use-ocamlfind -package ounit -package tgeoip  src/unittests.byte
-	ocamlrun ./_build/src/unittests.byte
+configure: setup.data
+setup.data: setup.ml
+	ocaml setup.ml -configure --enable-tests
+
+setup.ml: _oasis
+	oasis setup -setup-update dynamic
+
+doc install uninstall reinstall: setup.log
+	ocaml setup.ml -$@
+
+test: byte
+	LD_LIBRARY_PATH=_build/src/ ./unittests.byte
+
+.PHONY: configure all byte native doc install uninstall reinstall test
+
+.PHONY: dist tar
+dist tar: setup.ml
+	mkdir -p $(DIR)
+	for f in $(DISTFILES); do \
+	  cp -r --parents $$f $(DIR); \
+	done
+# Make a setup.ml independent of oasis:
+	cd $(DIR) && oasis setup
+	tar -zcvf $(TARBALL) $(DIR)
+	$(RM) -r $(DIR)
+
+.PHONY: clean distclean
+clean: setup.ml
+	ocaml setup.ml -clean
+	$(RM) $(TARBALL) iterate.dat
+
+distclean: setup.ml
+	ocaml setup.ml -distclean
+	$(RM) $(wildcard *.ba[0-9] *.bak *~ *.odocl)
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,3 +1,6 @@
+(* OASIS_START *)
+(* OASIS_STOP *)
+
 open Ocamlbuild_plugin
 open Command
 
@@ -5,15 +8,15 @@ open Command
 
 let os = Ocamlbuild_pack.My_unix.run_and_read "uname -s"
 
-let pkg_config flags package = 
-  let cmd tmp = 
-    Command.execute ~quiet:true & 
+let pkg_config flags package =
+  let cmd tmp =
+    Command.execute ~quiet:true &
     Cmd( S [ A "pkg-config"; A ("--" ^ flags); A package; Sh ">"; A tmp]);
     List.map (fun arg -> A arg) (string_list_of_file tmp)
   in
   with_temp_file "pkgconfig" "pkg-config" cmd
-       
-let pkg_config_lib ~lib ~has_lib ~stublib = 
+
+let pkg_config_lib ~lib ~has_lib ~stublib =
   let cflags = (A has_lib) :: pkg_config "cflags" lib in
   let stub_l = [A (Printf.sprintf "-l%s" stublib)] in
   let libs_l = pkg_config "libs-only-l" lib in
@@ -28,16 +31,18 @@ let pkg_config_lib ~lib ~has_lib ~stublib =
   let lib_flags = List.map (make_opt "-cclib") libs_l in
   let link_flags = List.map (make_opt "-ccopt") (linker @ libs_L) in
   let stublib_flags = List.map (make_opt "-dllib") stub_l  in
-  let tag = Printf.sprintf "use_%s" lib in
-  flag ["c"; "ocamlmklib"; tag] (S mklib_flags);
-  flag ["c"; "compile"; tag] (S compile_flags); 
-  flag ["link"; "ocaml"; tag] (S (link_flags @ lib_flags));
-  flag ["link"; "ocaml"; "library"; "byte"; tag] (S stublib_flags)
 
-let () = 
-  dispatch begin function
-  | After_rules ->
-      pkg_config_lib ~lib:"geoip" ~has_lib:"-DHAS_GEOIP" ~stublib:"tgeoip"
-  | _ -> ()
-  end
-    
+  flag ["c"; "ocamlmklib"] (S mklib_flags);
+  flag ["c"; "compile"] (S compile_flags);
+  flag ["link"; "ocaml"] (S (link_flags @ lib_flags));
+  flag ["link"; "ocaml"; "library"; "byte"] (S stublib_flags)
+
+let () =
+  let dispatch_pkg_config = function
+    | After_rules ->
+       pkg_config_lib ~lib:"geoip" ~has_lib:"-DHAS_GEOIP"
+                      ~stublib:"tgeoip_stubs"
+    | _ -> ()
+  in
+  dispatch (MyOCamlbuildBase.dispatch_combine
+              [dispatch_pkg_config; dispatch_default])
